1260618ef0c93579f8824002695aec488833a766
andrehahn05 <andrehahn05@gmail.com>
2021-10-21 21:18:56 -0400

    #

---
+++ src/modules/products/services/ListProducService.ts
@@ -0,0 +1,14 @@
+import { ProductRepository } from './../typeorm/repositories/ProductsRepository';
+import { getCustomRepository } from 'typeorm';
+import Product from '../typeorm/entities/Product';
+
+class ListProductService {
+  public async execute(): Promise<Product[]> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const product = productsRepository.find();
+
+    return product;
+  }
+}
+export default ListProductService;
