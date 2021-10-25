d06e76e3a3188145f2aff834712cf40a49942ce9
andrehahn05 <andrehahn05@gmail.com>
2021-10-25 16:14:43 -0400

    #

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
@@ -11,6 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(routes);
+app.use(errors());
 
 app.use(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
