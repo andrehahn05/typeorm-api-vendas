25b408d694b634b4b013553b67cfbf7aa8b4af81
andrehahn05 <andrehahn05@gmail.com>
2021-11-13 19:44:43 -0400

    #

--- src/modules/products/services/ListProducService.ts
+++ src/modules/products/services/ListProducService.ts
@@ -1,12 +1,17 @@
 import { ProductRepository } from './../typeorm/repositories/ProductsRepository';
 import { getCustomRepository } from 'typeorm';
 import Product from '../typeorm/entities/Product';
+import RedisCache from '@shared/cache/RedisCache';
 
 class ListProductService {
   public async execute(): Promise<Product[]> {
     const productsRepository = getCustomRepository(ProductRepository);
 
+    const redisCache = new RedisCache();
+
     const product = productsRepository.find();
+
+    await redisCache.save('teste', 'teste');
 
     return product;
   }
