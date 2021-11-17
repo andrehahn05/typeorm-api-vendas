d1cd2103741a40815c5741c358467391acb3087a
andrehahn05 <andrehahn05@gmail.com>
2021-11-17 19:22:32 -0400

    #

--- src/modules/customers/infra/typeorm/repositories/CustomersRepository.ts
+++ src/modules/customers/infra/typeorm/repositories/CustomersRepository.ts
@@ -1,32 +1,66 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { ICreateCustomer } from '@modules/customers/domain/models/ICreateCustomer';
+import { ICustomersRepository } from '@modules/customers/domain/repositories/ICustomersRepository';
+import { getRepository, Repository } from 'typeorm';
 import Customer from '../entities/Customer';
 
-@EntityRepository(Customer)
-class CustomersRepository extends Repository<Customer> {
+class CustomersRepository implements ICustomersRepository {
+  private repository: Repository<Customer>;
+
+  constructor() {
+    this.repository = getRepository(Customer);
+  }
+
+  public async create({ name, email }: ICreateCustomer): Promise<Customer> {
+    const customer = this.repository.create({ name, email });
+
+    await this.repository.save(customer);
+
+    return customer;
+  }
+
+  public async save(customer: Customer): Promise<Customer> {
+    await this.repository.save(customer);
+
+    return customer;
+  }
+
+  public async remove(customer: Customer): Promise<void> {
+    await this.repository.remove(customer);
+  }
+
+  public async findAll(): Promise<Customer[] | undefined> {
+    const customers = await this.repository.find();
+
+    return customers;
+  }
+
   public async findByName(name: string): Promise<Customer | undefined> {
-    const customer = await this.findOne({
+    const customer = await this.repository.findOne({
       where: {
         name,
       },
     });
+
     return customer;
   }
 
   public async findById(id: string): Promise<Customer | undefined> {
-    const customer = await this.findOne({
+    const customer = await this.repository.findOne({
       where: {
         id,
       },
     });
+
     return customer;
   }
 
   public async findByEmail(email: string): Promise<Customer | undefined> {
-    const customer = await this.findOne({
+    const customer = await this.repository.findOne({
       where: {
         email,
       },
     });
+
     return customer;
   }
 }
