92899e1c57c4f516c3387fd751e2b2aa628c6799
andrehahn05 <andrehahn05@gmail.com>
2022-11-19 22:19:25 -0400

    ref dataSource

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,38 @@
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+import { errors } from 'celebrate';
+import { pagination } from 'typeorm-pagination';
+import routes from './routes';
+import AppError from '@shared/errors/AppError';
+import '@shared/infra/typeorm';
+import '@shared/container';
+import rateLimiter from '@shared/infra/http/middlewares/rateLimiter';
+
+const app = express();
+app.use(express.json());
+app.use(rateLimiter);
+app.use(cors());
+app.use(pagination);
+app.use(routes);
+app.use(errors());
+
+app.use(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+    console.log(error);
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
+
+export default app;
--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,3 +1,39 @@
-import { createConnection } from 'typeorm';
+import { DataSource } from 'typeorm';
 
-createConnection();
+import User from '@modules/users/infra/typeorm/entities/User';
+import UserToken from '@modules/users/infra/typeorm/entities/UserToken';
+import Customer from '@modules/customers/infra/typeorm/entities/Customer';
+import Order from '@modules/orders/infra/typeorm/entities/Order';
+import OrdersProducts from '@modules/orders/infra/typeorm/entities/OrdersProducts';
+import Product from '@modules/products/infra/typeorm/entities/Product';
+
+import { CreateProducts1607437608841 } from './migrations/1634680581404-CreateProducts';
+import { CreateUsers1635272308978 } from './migrations/1635272308978-CreateUsers';
+import { CreateUserTokens1635813879933 } from './migrations/1635813879933-CreateUserTokens';
+import { CreateCustomers1636203422913 } from './migrations/1636203422913-CreateCustomers';
+import { CreateOrdes1636242606915 } from './migrations/1636242606915-CreateOrdes';
+import { addCustomerIdOrders1636244422216 } from './migrations/1636244422216-addCustomerIdOrders';
+import { CreateOrdersProducts1636330060997 } from './migrations/1636330060997-CreateOrdersProducts';
+import { addOrderIdToOrdersProducts1636330831712 } from './migrations/1636330831712-addOrderIdToOrdersProducts';
+import { addProductIdToOrdesProducts1636332695996 } from './migrations/1636332695996-addProductIdToOrdesProducts';
+
+export const dataSource = new DataSource({
+  type: 'postgres',
+  host: 'db',
+  port: 5432,
+  username: 'postgres',
+  password: 'docker',
+  database: 'apivendas',
+  entities: [User, UserToken, Customer, Order, OrdersProducts, Product],
+  migrations: [
+    CreateProducts1607437608841,
+    CreateUsers1635272308978,
+    CreateUserTokens1635813879933,
+    CreateCustomers1636203422913,
+    CreateOrdes1636242606915,
+    addCustomerIdOrders1636244422216,
+    CreateOrdersProducts1636330060997,
+    addOrderIdToOrdersProducts1636330831712,
+    addProductIdToOrdesProducts1636332695996,
+  ],
+});
