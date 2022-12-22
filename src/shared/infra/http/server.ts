097c2a5491be54b566ccf726feddc1b995d2b5d1
andrehahn05 <andrehahn05@gmail.com>
2022-12-22 01:24:51 -0400

    ending docker refactoring and conflict

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,7 +4,7 @@ import app from './app';
 import { dataSource } from '../typeorm';
 
 dataSource.initialize().then(() => {
-  app.listen(process.env.PORT || 3333, () => {
-    console.log(`Server started on port ${process.env.PORT || 3333}!`);
+  app.listen(process.env.PORT || 3001, () => {
+    console.log(`Server started on port ${process.env.PORT || 3001}!`);
   });
 });
